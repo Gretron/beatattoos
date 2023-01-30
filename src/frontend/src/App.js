ebe9f4a5e7db822e7e4a794c5f3432975249fac5
David Ano-Trudeau <89054564+Gretron@users.noreply.github.com>
2023-01-30 11:07:48 -0500

    chore(frontend): add routing base

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,19 +1,29 @@
+// #region Imports
+
+// Modules
 import React from "react";
 import { ReactDOM } from "react";
 
 // Components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 // Pages
 import Home from "./pages/Home";
 
+// #endregion
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
