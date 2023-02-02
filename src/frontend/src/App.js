7ea0eb88c3f0ff14cf297105d78f377c795eacac
David Ano-Trudeau <89054564+Gretron@users.noreply.github.com>
2023-02-01 19:48:19 -0500

    feat(frontend/app): add scrollsmoother to all pages

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -3,6 +3,8 @@
 // Modules
 import React from "react";
 import { ReactDOM } from "react";
+import { gsap } from "gsap";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 
 // Components
 import Header from "./components/Header";
@@ -13,9 +15,39 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 
+// Hooks
+import { useEffect, useRef } from "react";
+
 // #endregion
 
 function App() {
+  const loaded = useRef(false);
+
+  useEffect(() => {
+    // If Component Already Loaded...
+    if (loaded.current) {
+      // Skip
+      return; /*() => {
+        let st = ScrollSmoother.getById("timeline");
+
+        if (st) st.kill();
+      };*/
+    }
+
+    // Else Play Animation
+    loaded.current = true;
+
+    gsap.registerPlugin(ScrollSmoother);
+
+    const sm = ScrollSmoother.create({
+      wrapper: ".root",
+      content: ".App",
+      smooth: 1,
+      smoothTouch: 0.1,
+      effects: true,
+    });
+  }, []);
+
   return (
     <div className="App">
       <BrowserRouter>
