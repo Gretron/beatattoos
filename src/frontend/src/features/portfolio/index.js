108288ef7b42e86793987512a6ea1d3e797cae5d
David Ano-Trudeau <89054564+Gretron@users.noreply.github.com>
2023-02-04 13:58:28 -0500

    refactor(frontend/portfolio): rearranged portfolio exports

--- src/frontend/src/features/portfolio/index.js
+++ src/frontend/src/features/portfolio/index.js
@@ -1,4 +1,5 @@
+import portfolio from "./assets/css/portfolio.css";
 import Gallery from "./components/Gallery";
 import Timeline from "./components/Timeline";
 
-export { Gallery, Timeline };
+export { portfolio, Gallery, Timeline };
