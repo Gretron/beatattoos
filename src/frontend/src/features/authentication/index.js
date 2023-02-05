d6845a825ee83bc79603caa78e0765f14ffe0c23
Gretron <89054564+Gretron@users.noreply.github.com>
2023-02-05 15:57:36 -0500

    refactor(frontend/authentication): make exports go through index file

---
+++ src/frontend/src/features/authentication/index.js
@@ -0,0 +1,7 @@
+import loginStyles from "./assets/css/login.css";
+import LoginForm from "./components/LoginForm";
+
+import registerStyles from "./assets/css/register.css";
+import RegisterForm from "./components/RegisterForm";
+
+export { loginStyles, LoginForm, registerStyles, RegisterForm };
