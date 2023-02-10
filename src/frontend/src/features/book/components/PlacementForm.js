91e27d70bcbf33dd259d917a5183ed4cc13ef23f
David Ano-Trudeau <89054564+Gretron@users.noreply.github.com>
2023-02-10 00:43:50 -0500

    frag(frontend/book): refine placement form 3d base

--- src/frontend/src/features/book/components/PlacementForm.js
+++ src/frontend/src/features/book/components/PlacementForm.js
@@ -1,37 +1,118 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, Suspense } from "react";
 import { useOutletContext } from "react-router-dom";
 
-import {
-  PointLight,
-  AmbientLight,
-  BoxGeometry,
-  Mesh,
-  MeshStandardMaterial,
-  PerspectiveCamera,
-  Scene,
-  WebGLRenderer,
-  ACESFilmicToneMapping,
-  sRGBEncoding,
-  CanvasTexture,
-  NearestFilter,
-  MeshBasicMaterial,
-  Texture,
-  MeshLambertMaterial,
-  RepeatWrapping,
-  MirroredRepeatWrapping,
-  Vector3,
-  Raycaster,
-  Vector2,
-  MathUtils,
-} from "three";
+import { useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+import * as THREE from "three";
 
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
+import { useLoader, useThree } from "@react-three/fiber";
+import { useGLTF, OrbitControls } from "@react-three/drei";
+import { DecalGeometry } from "three/examples/jsm/geometries/DecalGeometry.js";
+
 import placementStyles from "../assets/css/placement.css";
 
 const PlacementForm = () => {
+  const [scene, setScene] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [renderer, setRenderer] = useState(null);
+  const [decal, setDecal] = useState(null);
+  const controls = useRef();
+
   let context = useOutletContext();
+
+  function Box(props) {
+    // This reference gives us direct access to the THREE.Mesh object
+    const ref = useRef();
+    // Hold state for hovered and clicked events
+    const [hovered, hover] = useState(false);
+    const [clicked, click] = useState(false);
+    // Subscribe this component to the render-loop, rotate the mesh every frame
+    useFrame((state, delta) => (ref.current.rotation.x += delta));
+    // Return the view, these are regular Threejs elements expressed in JSX
+    return (
+      <mesh
+        {...props}
+        ref={ref}
+        scale={clicked ? 1.5 : 1}
+        onClick={(event) => click(!clicked)}
+        onPointerOver={(event) => hover(true)}
+        onPointerOut={(event) => hover(false)}
+      >
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+      </mesh>
+    );
+  }
+
+  function Model({ ...props }) {
+    const pointer = new THREE.Vector2();
+
+    function handlePointerOver(e) {
+      let container = document.getElementById("placement-canvas");
+      let rect = container.getBoundingClientRect();
+
+      pointer.x = ((e.clientX - rect.left) / container.offsetWidth) * 2 - 1;
+      pointer.y = -((e.clientY - rect.top) / container.offsetHeight) * 2 + 1;
+
+      var raycaster = new THREE.Raycaster();
+
+      raycaster.setFromCamera(pointer, camera);
+      const hits = raycaster.intersectObjects(scene.children);
+
+      if (!hits.length) return;
+
+      const position = hits[0].point.clone();
+      const eye = position.clone();
+      eye.add(hits[0].face.normal);
+
+      const rotation = new THREE.Matrix4();
+      rotation.lookAt(eye, position, THREE.Object3D.DEFAULT_UP);
+      const euler = new THREE.Euler();
+      euler.setFromRotationMatrix(rotation);
+
+      const decalGeometry = new DecalGeometry(
+        hits[0].object,
+        hits[0].point,
+        euler,
+        new THREE.Vector3(0.5, 0.5, 0.5)
+      );
+      const decalMaterial = new THREE.MeshStandardMaterial({
+        color: 0xf05d23,
+        depthTest: true,
+        depthWrite: true,
+        polygonOffset: true,
+        polygonOffsetFactor: -4,
+      });
+
+      const newDecal = new THREE.Mesh(decalGeometry, decalMaterial);
+      newDecal.receiveShadow = true;
+
+      scene.remove(decal);
+      scene.add(newDecal);
+
+      setDecal(newDecal);
+
+      console.log(hits);
+    }
+
+    const group = useRef();
+    const { nodes, materials } = useGLTF("/models/model.gltf");
+
+    return (
+      <group ref={group} {...props} dispose={null}>
+        <mesh
+          geometry={nodes["BaseMesh_Low_Res-Detailed_Male-Femalelwo"].geometry}
+          material={props.material}
+          onClick={(event) => handlePointerOver(event)}
+          rotation={[Math.PI / 2, 0, 0]}
+        />
+      </group>
+    );
+  }
 
   // Component Loaded Flag
   const loaded = useRef(false);
@@ -48,134 +129,34 @@ const PlacementForm = () => {
 
     context.setHeader("select placement of tattoo");
     context.setNextStep("location");
-    // context.setPreviousStep("type/select");
-
-    const scene = new Scene();
-    const camera = new PerspectiveCamera(75, 4 / 2, 0.1, 1000);
-
-    camera.position.z = 5;
-
-    var canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d");
-    canvas.width = 500;
-    canvas.height = 500;
-
-    ctx.fillStyle = "green";
-    ctx.fillRect(0, 0, 250, 500);
-    ctx.fillRect(0, 250, 250, 250);
-    ctx.fillRect(250, 0, 250, 500);
-
-    let texture = new CanvasTexture(canvas);
-    let material2 = new MeshStandardMaterial({ map: texture });
-
-    const cube = new Mesh();
-    const geometry = new BoxGeometry();
-
-    cube.geometry = geometry;
-    cube.material = material2;
-    cube.position.x = -2.5;
-
-    const gltfLoader = new GLTFLoader();
-    gltfLoader.load(
-      "/models/untitled1.gltf",
-      (gltf) => {
-        gltf.scene.traverse((child) => {
-          if (child.isMesh) {
-            child.material = material2;
-          }
-        });
-
-        gltf.scene.scale.set(2, 2, 2);
-        gltf.scene.position.set(0, -2, 0);
-
-        gltf.scene.name = "1";
-
-        scene.add(gltf.scene);
-      },
-      function (xhr) {
-        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-      },
-      function (error) {
-        console.log("An error happened");
-      }
-    );
-
-    scene.add(cube);
-
-    const ambientLight = new AmbientLight();
-    scene.add(ambientLight);
-
-    const pointLight = new PointLight();
-    pointLight.position.set(10, 10, 10);
-    scene.add(pointLight);
-
-    const container = document.getElementById("placement-canvas");
-
-    const renderer = new WebGLRenderer({ alpha: true, antialias: true });
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.toneMapping = ACESFilmicToneMapping;
-    renderer.outputEncoding = sRGBEncoding;
-    renderer.setSize(container.clientWidth, container.clientHeight);
-
-    if (container) {
-      container.appendChild(renderer.domElement);
-      console.log("hallo");
-    }
-
-    const raycaster = new Raycaster();
-    const pointer = new Vector2();
-
-    function onPointerMove(event) {
-      // calculate pointer position in normalized device coordinates
-      // (-1 to +1) for both components
-
-      var rect = event.target.getBoundingClientRect();
-
-      pointer.x = ((event.clientX - rect.left) / container.offsetWidth) * 2 - 1;
-
-      pointer.y =
-        -((event.clientY - rect.top) / container.offsetHeight) * 2 + 1;
-
-      console.log(event.clientX - rect.left);
-      // console.log(event.clientX);
-      console.log(pointer);
-    }
-
-    container.addEventListener("pointermove", onPointerMove);
-
-    function onWindowResize() {
-      camera.aspect = container.clientWidth / container.clientHeight;
-
-      camera.updateProjectionMatrix();
-
-      renderer.setSize(container.clientWidth, container.clientHeight);
-    }
-
-    function animate() {
-      requestAnimationFrame(animate);
-
-      // update the picking ray with the camera and pointer position
-      raycaster.setFromCamera(pointer, camera);
-
-      // calculate objects intersecting the picking ray
-      const intersects = raycaster.intersectObjects(scene.children);
-
-      for (let i = 0; i < intersects.length; i++) {
-        intersects[i].object.material.color.set(0xff0000);
-        console.log(intersects);
-      }
-
-      /*
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      */
-      renderer.render(scene, camera);
-    }
-
-    animate();
   }, []);
 
-  return <div id="placement-canvas" className="placement-form"></div>;
+  const material = new THREE.MeshBasicMaterial();
+  material.color = new THREE.Color(0x493d08);
+
+  return (
+    <div id="placement-canvas" className="placement-form">
+      <Canvas
+        onCreated={({ camera, scene, gl }) => {
+          setScene(scene);
+          setCamera(camera);
+          camera.rotateZ(50);
+          setRenderer(gl);
+        }}
+      >
+        <axesHelper />
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 10, 10]} />
+        <OrbitControls
+          ref={controls}
+          autoRotate={false}
+          minPolarAngle={1.5}
+          maxPolarAngle={1.5}
+        />
+        <Model scale={[4, 4, 4]} material={material} />
+      </Canvas>
+    </div>
+  );
 };
 
 export default PlacementForm;
